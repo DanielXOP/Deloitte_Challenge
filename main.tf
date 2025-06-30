f3b2c74c47aaa7c779c0e56c160432aaeee964cd
DanielXOP <53865970+DanielXOP@users.noreply.github.com>
2025-06-30 15:52:23 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -56,7 +56,7 @@ resource "aws_security_group" "daniel-sg-mgmt" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["10.1.2.0/24"]
+    cidr_blocks = ["10.1.0.0/16"]
   }
 
   ingress {
@@ -64,7 +64,7 @@ resource "aws_security_group" "daniel-sg-mgmt" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["10.1.2.0/24"]
+    cidr_blocks = ["10.1.0.0/16"]
   }
 
   egress {
